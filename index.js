a772c66017896d47158e19811134fb927e8f368c
Ross Leitch <Ross@metamorphicsolutions.com>
2017-01-25 17:33:02 +1300

    Fucking bitwise

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var exports = {};
 var uuidFromName = function(name){
    var md5 = crypto.createHash('md5');
    md5.update(name);
-   var d = md5.digest('hex');
+   var d = md5.digest();
    d[6] &= 0x0F;
    d[6] |= 0x30;
    d[8] &= 0x3F;
    d[8] |= 0x80;
-   return formatAsUuid(d);
+   return formatAsUuid(d.toString('hex'));
 }
 
 var formatAsUuid = function(blob){
